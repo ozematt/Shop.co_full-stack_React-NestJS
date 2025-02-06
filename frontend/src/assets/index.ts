94d0b7d6b1c7eadc594dbb928fd919c2d3fdcf30
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-06 17:23:44 +0100

    added assets

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,66 @@
+import arrow from "../assets/Horizontal arrow.svg";
+
+import discount from "../assets/Discount.svg";
+import arrowWhite from "../assets/Arrow_white.svg";
+import cartIcon from "../assets/Cart.svg";
+import deleteIcon from "../assets/Delete.svg";
+import minus from "../assets/Minus.svg";
+import plus from "../assets/Plus.svg";
+
+import tick from "../assets/Green Tick.svg";
+
+import settings from "../assets/Settings.png";
+
+import hamburger from "../assets/Hamburger.svg";
+
+import user from "../assets/User.svg";
+import lock from "../assets/Lock.png";
+
+import arrowLeft from "../assets/Arrow-left.png";
+import arrowRight from "../assets/Arrow-right.png";
+
+// import closeBlack from "../assets/Close.svg";
+
+import userIcon from "../assets/Avatar.svg";
+import heroIMG from "../assets/hero_cart.png";
+
+import beauty from "../assets/Beauty.png";
+import clothes from "../assets/Clothes.png";
+import electronics from "../assets/Electronics.png";
+import kitchen from "../assets/Kitchen.png";
+import pay from "../assets/Pay.png";
+
+import lupeIcon from "../assets/Lupe_icon.png";
+import email from "../assets/Envelope.png";
+import close from "../assets/Close.svg";
+
+import darkIcon from "../assets/ThemeIcon_dark.png";
+
+export {
+  darkIcon,
+  close,
+  email,
+  lupeIcon,
+  pay,
+  beauty,
+  clothes,
+  electronics,
+  kitchen,
+  heroIMG,
+  arrow,
+  discount,
+  arrowWhite,
+  cartIcon,
+  deleteIcon,
+  minus,
+  plus,
+  tick,
+  settings,
+  hamburger,
+  user,
+  lock,
+  arrowLeft,
+  arrowRight,
+  // closeBlack,
+  userIcon,
+};
