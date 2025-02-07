190d8ae491938d814b3c1066ef970499a3ce9c22
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-07 18:54:50 +0100

    added user module files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { OrderModule } from './order/order.module';
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     OrderModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
