ee92ecef5463b4ad543c54b97f1f51be30c2abe5
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-07 18:52:37 +0100

    added method

--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -24,4 +24,30 @@ export class OrderService {
 
     return orders;
   }
+
+  async setOrder(user: User, dto: SetOrderDto) {
+    try {
+      const itemsArr = dto.orderItems.map((item) => ({
+        productName: item.productName,
+        image: item.image,
+        price: item.price,
+        quantity: item.quantity,
+      }));
+
+      const order = await this.prisma.order.create({
+        data: {
+          total: dto.total,
+          user_id: user.id,
+          orderItems: { create: itemsArr },
+        },
+        include: {
+          orderItems: true,
+        },
+      });
+      return order;
+    } catch (error) {
+      console.error('Error creating order:', error);
+      throw new HttpException('Cos nie tak', HttpStatus.BAD_REQUEST);
+    }
+  }
 }
