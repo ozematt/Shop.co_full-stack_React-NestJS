77307e9983ff1a69d7bf80dd2a45beb0ca606b12
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-07 19:01:35 +0100

    added user method

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { GetUser } from 'src/auth/decorator';
 import { User } from '@prisma/client';
 import { UserService } from './user.service';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
