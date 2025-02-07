894f6e90f2dbfd473aebbc422e0fe56cd0a1ae42
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-07 10:55:07 +0100

    added auth logic

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,72 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private jwt: JwtService,
+    private config: ConfigService,
+    private prisma: PrismaService,
+  ) {}
+
+  async register(dto: AuthDto) {
+    const hash = await bcrypt.hash(dto.password, 10);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+
+      return this.signToken(user.id, user.email);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('User already exist');
+        }
+      }
+      throw error;
+    }
+  }
+
+  async login(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    const isMatch = await bcrypt.compare(dto.password, user.password);
+
+    if (!isMatch) throw new ForbiddenException('Credentials incorrect');
+
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const tokenPayload = {
+      sub: userId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(tokenPayload, {
+      expiresIn: '1d',
+      secret: secret,
+    });
+
+    return { access_token: token };
+  }
+}
