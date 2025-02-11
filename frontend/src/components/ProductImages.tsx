0ec42f4c228a8083c668db7dceda60be8cc2f686
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-11 20:01:59 +0100

    addded logic

--- frontend/src/components/ProductImages.tsx
+++ frontend/src/components/ProductImages.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { type ProductImagesProps } from '../lib/types';
 import { SkeletonMainImage } from '.';
+import SkeletonAsideImages from './SkeletonAsideImages';
 
 const ProductImages = ({ images }: ProductImagesProps) => {
   //
@@ -16,19 +17,11 @@ const ProductImages = ({ images }: ProductImagesProps) => {
   return (
     <div className="mx-auto flex w-full max-w-[610px] gap-[14px] max-[1430px]:flex-col-reverse">
       {' '}
-      <div className="relative flex justify-between gap-[14px] min-[1430px]:flex-col">
-        {/* <div className="absolute h-full max-h-[106px] animate-pulse" /> */}
-
-        {images.slice(0, 3).map((img) => (
-          <img
-            key={img}
-            src={img}
-            alt="image"
-            onClick={() => setBigImg(img)}
-            className="h-full max-h-[106px] max-w-[111px] rounded-[20px] bg-grayBG object-contain ring-black hover:ring-1 md:max-h-[167px] md:max-w-[152px] dark:bg-zinc-900 dark:ring-white"
-          />
-        ))}
-      </div>
+      <SkeletonAsideImages
+        images={images}
+        onClick={setBigImg}
+        loaded={loaded}
+      />
       {/* main IMG */}
       <SkeletonMainImage
         image={bigImg}
--- frontend/src/components/SkeletonAsideImages.tsx
+++ frontend/src/components/SkeletonAsideImages.tsx
@@ -12,23 +12,25 @@ const SkeletonAsideImages = ({
 }: SkeletonAsideImagesProps) => {
   return (
     <div className="relative flex justify-between gap-[14px] min-[1430px]:flex-col">
-      {!loaded && (
-        <div className="absolute h-full max-h-[106px] animate-pulse">
-          <div className="h-full w-full bg-black"></div>
-          <div className="h-full w-full bg-black"></div>
-          <div className="h-full w-full bg-black"></div>
-        </div>
+      {!loaded ? (
+        <>
+          <div className="h-full max-h-[106px] max-w-[111px] rounded-[20px] bg-black md:max-h-[167px] md:max-w-[152px]" />
+          <div className="h-full max-h-[106px] max-w-[111px] rounded-[20px] bg-black md:max-h-[167px] md:max-w-[152px]" />
+          <div className="h-full max-h-[106px] max-w-[111px] rounded-[20px] bg-black md:max-h-[167px] md:max-w-[152px]" />
+        </>
+      ) : (
+        images
+          .slice(0, 3)
+          .map((image) => (
+            <img
+              key={image}
+              src={image}
+              alt="image"
+              onClick={() => onClick(image)}
+              className="h-full max-h-[106px] max-w-[111px] rounded-[20px] bg-grayBG object-contain ring-black hover:ring-1 md:max-h-[167px] md:max-w-[152px] dark:bg-zinc-900 dark:ring-white"
+            />
+          ))
       )}
-
-      {images.slice(0, 3).map((image) => (
-        <img
-          key={image}
-          src={image}
-          alt="image"
-          onClick={() => onClick(image)}
-          className="h-full max-h-[106px] max-w-[111px] rounded-[20px] bg-grayBG object-contain ring-black hover:ring-1 md:max-h-[167px] md:max-w-[152px] dark:bg-zinc-900 dark:ring-white"
-        />
-      ))}
     </div>
   );
 };
