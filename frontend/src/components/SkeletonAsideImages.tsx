4b174224f1384e231e06179db620051b933386c0
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-11 20:02:31 +0100

    added logic

--- frontend/src/components/SkeletonAsideImages.tsx
+++ frontend/src/components/SkeletonAsideImages.tsx
@@ -11,7 +11,27 @@ const SkeletonAsideImages = ({
   loaded,
 }: SkeletonAsideImagesProps) => {
   return (
-
+    <div className="relative flex justify-between gap-[14px] min-[1430px]:flex-col">
+      {!loaded ? (
+        <>
+          <div className="h-full max-h-[106px] max-w-[111px] rounded-[20px] bg-black md:max-h-[167px] md:max-w-[152px]" />
+          <div className="h-full max-h-[106px] max-w-[111px] rounded-[20px] bg-black md:max-h-[167px] md:max-w-[152px]" />
+          <div className="h-full max-h-[106px] max-w-[111px] rounded-[20px] bg-black md:max-h-[167px] md:max-w-[152px]" />
+        </>
+      ) : (
+        images
+          .slice(0, 3)
+          .map((image) => (
+            <img
+              key={image}
+              src={image}
+              alt="image"
+              onClick={() => onClick(image)}
+              className="h-full max-h-[106px] max-w-[111px] rounded-[20px] bg-grayBG object-contain ring-black hover:ring-1 md:max-h-[167px] md:max-w-[152px] dark:bg-zinc-900 dark:ring-white"
+            />
+          ))
+      )}
+    </div>
   );
 };
 
