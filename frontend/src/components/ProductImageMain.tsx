018b05986c29835dcbcb4955b81ff8c71ba0035f
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-12 19:09:07 +0100

    edited styles

--- frontend/src/components/ProductImageMain.tsx
+++ frontend/src/components/ProductImageMain.tsx
@@ -10,12 +10,12 @@ const ProductImageMain = ({ image, onLoad, loaded }: ProductImageMainProps) => {
   //
   //UI
   return (
-    <div className="relative flex h-full w-full max-w-[644px] items-center justify-center overflow-hidden rounded-[20px] bg-grayBG object-contain max-xl:max-h-[330px] dark:bg-zinc-900">
+    <div className="relative flex h-full max-h-[500px] w-full max-w-[644px] items-center justify-center overflow-hidden rounded-[20px] bg-grayBG object-contain max-xl:max-h-[330px] dark:bg-zinc-900">
       {!loaded && <SkeletonMainImage />}
       <img
         src={image}
         alt="main product image"
-        className={`h-full w-full object-cover transition-opacity duration-500 ${
+        className={`h-[100%] w-[100%] object-contain transition-opacity duration-500 ${
           loaded ? 'opacity-100' : 'opacity-0'
         }`}
         onLoad={onLoad}
--- frontend/src/components/ProductImagesAside.tsx
+++ frontend/src/components/ProductImagesAside.tsx
@@ -14,14 +14,14 @@ const ProductImagesAside = ({
   //
   ////UI
   return (
-    <div className="flex h-[100px] w-full justify-between gap-[12px] xl:h-[500px] xl:w-[168px] xl:flex-col">
+    <div className="flex h-[100px] w-full justify-between gap-[12px] xl:h-[500px] xl:w-[180px] xl:flex-col">
       {!loaded ? (
         <SkeletonImagesAside />
       ) : (
         images.slice(0, 3).map((image, index) => (
           <div
             key={index}
-            className="grid h-full max-h-[156px] w-full place-items-center rounded-xl bg-grayBG hover:ring-2 hover:ring-black dark:bg-zinc-900"
+            className="grid w-full place-items-center overflow-hidden rounded-xl bg-grayBG hover:ring-2 hover:ring-black lg:h-[158px] dark:bg-zinc-900"
           >
             {' '}
             <img
