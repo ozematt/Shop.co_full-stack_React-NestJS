4c7b994dfd23cc7e11b765f0596e0d0118f0c2b8
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-12 14:57:28 +0100

    refactor

--- frontend/src/components/SortByMenu.tsx
+++ frontend/src/components/SortByMenu.tsx
@@ -1,24 +1,25 @@
-import { useState } from "react";
-import { sortingOptions } from "../constants";
-import { AppDispatch, useAppDispatch } from "../redux/store";
-import { SortMethod } from "../lib/types";
-import { addSortMethod } from "../redux/productsSlice";
+import { useState } from 'react';
+import { sortingOptions } from '../constants';
+import { AppDispatch, useAppDispatch } from '../redux/store';
+import { SortMethod } from '../lib/types';
+import { addSortMethod } from '../redux/productsSlice';
 
 const SortByMenu = () => {
+  //
+  ////DATA
   const dispatch: AppDispatch = useAppDispatch();
-
   const [open, setOpen] = useState(false);
-  const [sortBy, setSortBy] = useState("Alphabetical");
 
+  ////LOGIC
   const handleSortChange = (option: SortMethod) => {
-    setSortBy(option);
     setOpen(false);
     dispatch(addSortMethod(option));
   };
 
+  ////UI
   return (
     <ul
-      className={`absolute right-[-12px] text-lg ${open ? "block" : "hidden"} top-[50px] z-10 bg-white text-center ring-1 ring-black`}
+      className={`absolute right-[-12px] text-lg ${open ? 'block' : 'hidden'} top-[50px] z-10 bg-white text-center ring-1 ring-black`}
     >
       <div className="absolute right-4 top-[-7px] z-40 h-3 w-3 rotate-45 border-l-[1px] border-t-[1px] border-black bg-white" />
       {sortingOptions.map((option) => (
