5f5b847c85a07055229fa9da83e414ed765591f1
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-13 10:07:15 +0100

    added sticky nav

--- frontend/src/sections/Nav.tsx
+++ frontend/src/sections/Nav.tsx
@@ -4,13 +4,13 @@ import {
   NavLinks,
   Logo,
   NavIcons,
-} from "../components";
+} from '../components';
 
 const Nav = () => {
   //
   ////UI
   return (
-    <nav className="max-container flex h-[96px] items-center px-4 max-[838px]:justify-between sm:px-[100px]">
+    <nav className="max-container sticky top-0 z-50 flex h-[96px] items-center px-4 backdrop-blur-[15px] max-[838px]:justify-between sm:px-[100px]">
       <div className="flex items-center">
         <HamburgerMenu />
         <Logo />
