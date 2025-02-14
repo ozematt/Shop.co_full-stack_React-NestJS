d4865bad7655eac5cf04294705862d1c8635e861
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-14 10:46:06 +0100

    refactor

--- frontend/src/components/CheckoutPayButton.tsx
+++ frontend/src/components/CheckoutPayButton.tsx
@@ -8,7 +8,7 @@ const CheckoutPayButton = ({ onClick }: CheckoutPayButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="mt-6 w-full max-w-[457px] rounded-full bg-black py-[19px] font-satoshi font-medium text-white transition duration-100 ease-in-out hover:scale-95 max-sm:text-sm dark:bg-white dark:text-black"
+      className="mt-6 w-full max-w-[457px] self-center rounded-full bg-black py-[19px] font-satoshi font-medium text-white transition duration-100 ease-in-out hover:scale-95 max-sm:text-sm dark:bg-white dark:text-black"
     >
       PAY
     </button>
