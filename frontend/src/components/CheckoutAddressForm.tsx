191a23856525ba0c73cfd84e2b9c20423cd432c2
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-21 15:45:04 +0100

    added zod schema

--- frontend/src/components/CheckoutAddressForm.tsx
+++ frontend/src/components/CheckoutAddressForm.tsx
@@ -2,6 +2,25 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
+const zipCodeValidation = new RegExp(/^[0-9]{2}-[0-9]{3}/);
+const fullNameValidation = new RegExp(/^\b[a-zA-Z]{2,}\b \b[a-zA-Z]{2,}\b$/);
+
+const checkoutAddressFormSchema = z.object({
+  fullName: z
+    .string()
+    .regex(
+      fullNameValidation,
+      'Full name must contain two words with at least 2 characters',
+    ),
+  street: z.string().min(2, 'Street must be at least 2 characters'),
+  houseNumber: z.string().min(1, 'House number must be at least 1'),
+  city: z.string().min(2, 'City must be at least 2 characters'),
+  zipCode: z.string().regex(zipCodeValidation, 'Zip code must be XX-XXX'),
+  country: z.string().min(2, 'Country must be at least 2 characters'),
+});
+
+export type CheckoutAddressForm = z.infer<typeof checkoutAddressFormSchema>;
+
 const CheckoutAddressForm = () => {
   //
   ////DATA
@@ -11,8 +30,8 @@ const CheckoutAddressForm = () => {
     setError,
     clearErrors,
     formState: { errors, isSubmitting },
-  } = useForm<>({
-    resolver: zodResolver(),
+  } = useForm<CheckoutAddressForm>({
+    resolver: zodResolver(checkoutAddressFormSchema),
   });
 
   return <div>CheckoutAddressForm</div>;
