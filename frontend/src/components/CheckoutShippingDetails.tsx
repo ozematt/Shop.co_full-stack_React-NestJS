23a6b7ac4c708ea2fcf3755b7cfbd25161ec1bd2
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-22 14:59:18 +0100

    addde logic

--- frontend/src/components/CheckoutShippingDetails.tsx
+++ frontend/src/components/CheckoutShippingDetails.tsx
@@ -6,10 +6,11 @@ const CheckoutShippingDetails = () => {
   return (
     <div className="h-full w-full rounded-[20px] ring-1 ring-black ring-opacity-10 min-[1454px]:max-w-[715px] dark:ring-white">
       <div className="px-6 pb-[33px] pt-[20px]">
-        <div className="pb-1">
+        <div className="">
           <SectionSubtitle title="Shipping recipient details:" />
+          <p className="pt-6">SELECT ADDRESS</p>
         </div>
-        <div className="border-b-[1px] pt-5" />
+        <div className="border-b-[1px] pt-6" />
         <CheckoutAddress />
       </div>
     </div>
